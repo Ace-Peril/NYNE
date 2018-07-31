8d47abcda4d034d10de52963ef2906245cc0274e
Ace-Peril <35380593+Ace-Peril@users.noreply.github.com>
2018-07-30 17:56:49 -0700

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,10 @@
+var HTML = (canvasID, Width Height) {
+  this.width = width;
+  this.height = height;
+  this.canvas = null;
+  this.context = null;
+  this.canvas = document.getElementById(canvasID);
+  this.context = this.canvas.getContext("2d");
+  $(this.canvas).attr
+  $(
+}
